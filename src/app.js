705d5fb23970bd399b018b3b990ad4dadf2af9eb
Cedric Nicoloso <cedric.nicoloso@gmail.com>
2018-06-25 20:22:25 +0200

    Remove web server related code

--- src/app.js
+++ src/app.js
@@ -1,21 +1,11 @@
-const express = require('express')
-const morgan = require('morgan')
 const log = require('./logger')
 const { add } = require('./utils')
 
-const app = express()
-
-app.use(morgan('tiny'))
-
-app.get('/', (req, res) => {
-  log.debug('Handling request for endpoint: GET /')
+function run () {
   const result = add(2, 2)
-  res.send(`Hello World, result is ${result}`)
-})
+  log.info(`App has run: ${result}`)
+}
 
-app.post('/', express.json(), (req, res) => {
-  log.debug('Handling request for endpoint: POST /')
-  res.send(`You sent me ${JSON.stringify(req.body)}`)
-})
-
-module.exports = app
+module.exports = {
+  run,
+}
---
+++ test/utils.spec.js
@@ -0,0 +1,8 @@
+const { add } = require('../src/utils')
+
+describe('When calling add() with 2 and 2', () => {
+  test('It should answer 4', async () => {
+    const result = add(2, 2)
+    expect(result).toBe(4)
+  })
+})
